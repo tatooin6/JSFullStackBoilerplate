1803f5b660c2110e11045416aca6c0600b7876df
Antonio Pantoja <tato_fos@outlook.com>
2025-10-25 19:31:11 -0400

    Add base improvements to REST
    - Created POST request
    - Added JSON body support
    - Added db.json more data examples

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,27 +1,42 @@
 // R
-import fs from 'fs';
-import cors from 'cors';
-import express from 'express';
+import fs from "fs";
+import cors from "cors";
+import express from "express";
 const PORT = 3000;
 const app = express();
 // U
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 // E
-app.get('/users', (req, res) => {
-  const response = readDB()
+app.get("/users", (req, res) => {
+  const response = readDB();
   res.json(response);
-})
+});
+app.post("/users", (req, res) => {
+  const requestData = req.body;
+
+  try {
+    writeDB(requestData);
+    res.json({ status: "success", saved: requestData });
+    const current = readDB();
+    console.log(current)
+  } catch (err) {
+    console.error("something went wrong", err);
+    res.json({ status: "failed", sent: requestData });
+  }
+});
 // G
 function readDB() {
-  const data = fs.readFileSync('./db.json');
+  const data = fs.readFileSync("./db.json");
   return JSON.parse(data);
 }
 
 function writeDB(data) {
-  fs.writeFileSync('./db.json', JSON.stringify(data, 2, null));
+  const former = readDB();
+  fs.writeFileSync("./db.json", JSON.stringify([...former, data], 2, null));
 }
 // A
 app.listen(PORT, () => {
   console.log(`server started on http://localhost:${PORT}`);
-})
+});
