f49f815585bda4e9bffb89bf8d942e80e5a01aa9
Antonio Pantoja <tato_fos@outlook.com>
2025-10-25 22:12:58 -0400

    Include post from frontend

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -10,16 +10,36 @@ function App() {
     };
     getUsers();
   }, []);
-  const [count, setCount] = useState(0);
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmitNewUser = async () => {
+    const url = 'http://localhost:3000/users';
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name: userName, email})
+      });
+      console.log(response);
+    } catch (err) {
+      console.error('Unexpected error: ', err);
+    }
+  }
 
   return (
     <>
       <h1>APISIX - pruebas</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div>
+          <label htmlFor="userName">User Name</label>
+          <input type="text" name="userName" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+          <br/>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button onClick={handleSubmitNewUser}></button>
+        </div> 
         <ul>
           {
             users.map((user, index) => (
